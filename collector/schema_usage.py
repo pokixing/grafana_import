4f8e32dd65a5251262073e2bf45aef391b9f17b7
zheyuan.xing <zheyuan.xing@esgyn.cn>
2018-11-02 07:58:08 +0000

    Modified collector

--- collector/schema_usage.py
+++ collector/schema_usage.py
@@ -11,20 +11,20 @@ COLLECTION_INTERVAL = 30
 cmd = 'sqlci -q "select sum(store_file_size)/1024/1024 as store_size from table(cluster stats())"'
 
 def main():
-    global schema_usage
-    try:
-        while True:
- 	    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
- 	    stdout, stderr = p.communicate()
- 	    ts = int(time.time())
-	    index = stdout.split().index("STORE_SIZE")
-	    schema_usage = stdout.split()[index+2]
- 	    print("esgyn.schema.size %d %s" % (ts, schema_usage))
- 	    sys.stdout.flush()
- 	    time.sleep(COLLECTION_INTERVAL)
-
-    except:
-        print "Unexpected error:", sys.exc_info()[0]
+	global schema_usage
+	try:
+		while True:
+			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			stdout, stderr = p.communicate()
+			ts = int(time.time())
+			index = stdout.split().index("STORE_SIZE")
+			schema_usage = stdout.split()[index+2]
+			print("esgyn.schema.size %d %s" % (ts, schema_usage))
+			sys.stdout.flush()
+			time.sleep(COLLECTION_INTERVAL)
+	
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
 
 if __name__ == "__main__":
     sys.stdin.close()
--- collector/stfs_status.py
+++ collector/stfs_status.py
@@ -10,26 +10,26 @@ COLLECTION_INTERVAL = 30
 cmd = 'edb_pdsh -a "lsof +L1 2>/dev/null | grep /SCR"'
 
 def main():
-    try:
-        while True:
-	    STFS_status = None
-            p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            ts = int(time.time())
-	    stdout, stderr = p.communicate()
-	    if stdout:
-	        STFS_status = False
-	    elif not stdout:
-	        STFS_status = True
-
-	    if STFS_status:
-	        print("esgyn.stfs.running %d 1" % ts)
-	    else:
-	        print("esgyn.stfs.running %d 0" % ts)
-	    sys.stdout.flush()
-	    time.sleep(COLLECTION_INTERVAL)
-
-    except:
-	print "Unexpected error:", sys.exc_info()[0]
+	try:
+		while True:
+			STFS_status = None
+			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			ts = int(time.time())
+			stdout, stderr = p.communicate()
+			if stdout:
+				STFS_status = False
+			elif not stdout:
+				STFS_status = True
+				
+			if STFS_status:
+				print("esgyn.stfs.running %d 1" % ts)
+			else:
+				print("esgyn.stfs.running %d 0" % ts)
+			sys.stdout.flush()
+			time.sleep(COLLECTION_INTERVAL)
+	
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
 
 if __name__ == "__main__":
     sys.stdin.close()
