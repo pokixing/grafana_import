9ad65996d3d30efd35295738efce0597ec620682
zheyuan.xing <zheyuan.xing@esgyn.cn>
2018-11-05 05:03:49 +0000

    Add stfs_usage.py

---
+++ collector/stfs_usage.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import subprocess
+import sys
+import time
+import re
+import os
+
+COLLECTION_INTERVAL = 30
+cmd = '''echo `dtmci showenv STFS_HDD_LOCATION` | gawk -v OFS="\n" 'BEGIN{FS=":"} {$1=$1;if(NF==1) print $1; else print $1,$NF}' '''
+
+def main():
+        try:
+                while True:
+			stfs = []
+                        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                        ts = int(time.time())
+			stdout,stderr = p.communicate()
+			stdout = stdout.split()
+			for line in stdout:
+				df_cmd = "df -k " + line
+				du_cmd = "du -k --max-depth=0 " + line
+				
+				p1 = subprocess.Popen(df_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+				df_stdout,df_stderr = p1.communicate()
+				df_stdout = df_stdout.split('\n')[1].split()
+				filesys, filefree = df_stdout[0], df_stdout[3]
+				
+				p2 = subprocess.Popen(du_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+				du_stdout,du_stderr = p2.communicate()
+				stfsusage = du_stdout.split()[0]
+				stfs.append([line,filesys,stfsusage,filefree])
+			
+			stfs_usage,file_free = 0,0
+			file_sys = []
+			for sys in stfs:
+				stfs_usage += eval(sys[2])
+				if sys[1] in file_sys:
+					file_sys.append(sys[1])
+				elif sys[1] not in file_sys:
+					file_sys.append(sys[1])
+					file_free += eval(sys[3])
+			
+			stfs_prect = (float(stfs_usage)/file_free) * 100
+			print("esgyn.stfs.prect %d %.5s" % (ts,stfs_prect))
+                        time.sleep(COLLECTION_INTERVAL)
+
+        except:
+                print "Unexpected error:", sys.exc_info()[0]
+
+if __name__ == "__main__":
+    sys.stdin.close()
+    sys.exit(main())
+
